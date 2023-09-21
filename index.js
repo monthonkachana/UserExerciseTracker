d5be4c48097fa9d4e03516f74e07548fc0ff75e0
monthonkachana <maxmonthonkachana2@gmail.com>
2023-09-21 11:11:09 +0700

    end

--- index.js
+++ index.js
@@ -2,15 +2,127 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const mongoose = require('mongoose')
+const { Schema } = mongoose;
+//moongoatlas database id:
+// username : maxmonthonkachana2 pass :a3bM4ySgPKiUnA5H ip: 0.0.0.0/0
+const uri = 'mongodb+srv://maxmonthonkachana2:a3bM4ySgPKiUnA5H@freecodecamp.2syq8co.mongodb.net/exercise-tracker?retryWrites=true&w=majority';
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+//model user
+const UserSchema = new Schema({
+  username: String,
+});
+const User = mongoose.model("User", UserSchema);
+//model exercise 
+const ExerciseSchema = new Schema({
+  user_id: { type: String, required: true },
+  description: String,
+  duration: Number,
+  date: Date,
+});
+const Exercise = mongoose.model("Exercise", ExerciseSchema);
+
 
 app.use(cors())
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+app.get("/api/users", async (req, res) => {
+  // หาข้อมูลทั้งหมดใน db ดึงออกมา 
+  const users = await User.find({}).select("_id username")
+  if (!users) {
+    res.send("No users");
+  } else {
+    res.json(users)
+  }
+})
 
+app.post("/api/users", async (req, res) => {
+  console.log(req.body)
+  const userObj = new User({
+    username: req.body.username
+  })
 
+  try {
+    const user = await userObj.save()
+    console.log(user)
+    res.json(user)
+  } catch (err) {
+    console.log(err)
+  }
+
+})
+
+app.post("/api/users/:_id/exercises", async (req, res) => {
+  const id = req.params._id;
+  const { description, duration, date } = req.body;
+
+  try {
+    const user = await User.findById(id); // Correct method name is findById
+
+    if (!user) {
+      res.send("Could not find user");
+    } else {
+      const exerciseObj = new Exercise({
+        user_id: user._id,
+        description,
+        duration,
+        date: date ? new Date(date) : new Date(),
+      });
+
+      const exercise = await exerciseObj.save();
+
+      res.json({
+        _id: user._id,
+        username: user.username,
+        description: exercise.description, // Correct property name
+        duration: exercise.duration,
+        date: new Date(exercise.date).toDateString(),
+      });
+    }
+  } catch (err) {
+    console.error(err); // Use console.error for error messages
+    res.status(500).send("There was an error saving the exercise");
+  }
+});
+
+app.get("/api/users/:_id/logs", async (req, res) => {
+  const { form, to, limit } = req.query;
+  const id = req.params._id;
+  const user = await User.findById(id);
+  if (!user) {
+    res.send("Could not find user")
+    return;
+  }
+  let dateObj = {}
+  if (form) {
+    dateObj["$gte"] = new Date(form)
+  }
+  if (to) {
+    dateObj["$lte"] = new Date(to)
+  }
+  let filter = {
+    user_id: id
+  }
+  if (form || to) {
+    filter.date = dateObj;
+  }
+  const exercises = await Exercise.find(filter).limit(+limit ?? 500)
+  const log = exercises.map(e => ({
+    description: e.description,
+    duration: e.duration,
+    date: e.date.toDateString()
+  }))
+  res.json({
+    username: user.username,
+    count: exercises.length,
+    _id: user._id,
+    log
+  })
+})
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
